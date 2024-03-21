45da9181b86eaa17f0ae1ff6efb8fb04ef2f936a
JP-SALLAH <jeanpaulsallah@gmail.com>
2024-03-21 15:25:27 +0400

    Creation de la fonctionnalité Stock

--- script/caisse.c
+++ script/caisse.c
@@ -16,7 +16,10 @@ double RandDouble() {
 int main() {
 	double Paye;
 	double Montant = RandDouble();
-	double Billets[] = {2000.0, 1000.0, 500.0, 200.0, 100.0, 50.0, 25.0, 20.0, 10.0, 5.0, 1.0, 0.5, 0.2, 0.05}; 
+	double Billets[] = {2000.0, 1000.0, 500.0, 200.0, 100.0, 50.0, 25.0, 20.0, 10.0, 5.0, 1.0, 0.5, 0.2, 0.05};
+	int Stock[14];
+	for (int i = 0; i < 14; i++) Stock[i] = 40;
+	
 	
 	printf("Le montant a payé est %.2lf.\n", Montant);
 	printf("Veuillez indiquez le montant que vous allez donner : ");
@@ -32,20 +35,31 @@ int main() {
 	printf("Votre monnaie est de %.2lf.\n", aRendre);
 	
 	int i = 0;
-	while (aRendre > 1) {
-		if (aRendre >= Billets[i]) {
+	while (aRendre >= 0.05) {
+		if ((aRendre >= Billets[i]) && (Stock[i] > 0)) {
 			aRendre -= Billets[i];
+			Stock[i] -= 1;
 			Rendre[i] += 1;
 		}
 		else 
 			i++;
 	}
 	
+	printf("\n");
 	printf("On vous rendra : \n");
 	for (int i = 0; i < 14; i++) {
 		if (Rendre[i] != 0) {
-			printf("%i Billet de %.0lf.\n", Rendre[i], Billets[i]);
+			if (i < 7) printf("%i Billet de %.2lf.\n", Rendre[i], Billets[i]);
+			else printf("%i Pièce de %.2lf.\n", Rendre[i], Billets[i]);
 		}
 	}
+	
+	printf("\n");
+	printf("Il reste dans le stock :\n");
+	for (int i = 0; i < 14; i++) {
+		if (i < 7) printf("%i Billet de %.2lf.\n", Stock[i], Billets[i]);
+		else printf("%i Pièce de %.2lf.\n", Stock[i], Billets[i]);
+	}
+		
 
 }
